a846c2e49ae2eb4840c1f3f1f9d308078c008997
Arssi24 <arsentomash02@gmail.com>
2026-07-07 15:38:46 -0500

    Fix broken service worker caching

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pti-inspections-v1';
-const CORE_ASSETS = ['./', './index.html', './css/styles.css', './js/app.js', './js/db.js', './manifest.json'];
+const CACHE_NAME = 'pti-inspections-v2';
+const CORE_ASSETS = ['./', './index.html', './css/styles.css', './js/app.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)));
